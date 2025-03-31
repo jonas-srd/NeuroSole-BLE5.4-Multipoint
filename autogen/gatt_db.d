1aff5162fbea37b985318f993cc8f055e3d0308b
Jonas Schröder <84855849+jonas-srd@users.noreply.github.com>
2025-03-31 13:29:05 +0200

    Delete autogen directory

--- autogen/gatt_db.d
+++
@@ -1,5 +0,0 @@
-autogen/gatt_db.o: ../autogen/gatt_db.c \
- c:\users\schro\simplicitystudio\sdks\gecko_sdk\platform\common\toolchain\inc\sl_gcc_preinclude.h \
- c:\users\schro\simplicitystudio\sdks\gecko_sdk\protocol\bluetooth\inc\sli_bt_gattdb_def.h
-c:\users\schro\simplicitystudio\sdks\gecko_sdk\platform\common\toolchain\inc\sl_gcc_preinclude.h:
-c:\users\schro\simplicitystudio\sdks\gecko_sdk\protocol\bluetooth\inc\sli_bt_gattdb_def.h:
