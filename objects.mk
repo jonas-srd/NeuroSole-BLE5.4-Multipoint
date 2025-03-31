04e3eb026da761850ab9bac7532f79703378c08c
Jonas Schröder <84855849+jonas-srd@users.noreply.github.com>
2025-03-31 13:04:46 +0200

    Delete objects.mk

--- objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS := C:/Users/schro/SimplicityStudio/SDKs/gecko_sdk//protocol/bluetooth/bgcommon/lib/libbgcommon_efr32xg24_gcc_release.a C:/Users/schro/SimplicityStudio/SDKs/gecko_sdk//protocol/bluetooth/bgstack/ll/lib/libbluetooth_controller_efr32xg24_gcc_release.a C:/Users/schro/SimplicityStudio/SDKs/gecko_sdk//protocol/bluetooth/lib/libbluetooth_host_efr32xg24_gcc_release.a C:/Users/schro/SimplicityStudio/SDKs/gecko_sdk//platform/emdrv/nvm3/lib/libnvm3_CM33_gcc.a C:/Users/schro/SimplicityStudio/SDKs/gecko_sdk//platform/radio/rail_lib/autogen/librail_release/librail_config_bgm240pb32vna_gcc.a C:/Users/schro/SimplicityStudio/SDKs/gecko_sdk//platform/radio/rail_lib/autogen/librail_release/librail_module_efr32xg24_gcc_release.a
-
-LIBS := -lgcc -lc -lm -lnosys
-
